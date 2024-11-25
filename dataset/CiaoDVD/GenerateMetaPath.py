4391d373d8982a6ca8399a55f26c15e118df4315
xinlong feng <43529137+fxl951677676@users.noreply.github.com>
2024-11-25 18:05:35 +0800

    Update GenerateMetaPath.py

--- dataset/CiaoDVD/GenerateMetaPath.py
+++ dataset/CiaoDVD/GenerateMetaPath.py
@@ -1,76 +1,52 @@
- import numpy as np
+import numpy as np
 import scipy.sparse as sp
 import pickle
 import datetime
 from tqdm import tqdm
-
-print(datetime.datetime.now())
-
-np.random.seed(30)  #make generate the same metapath when running this code
+np.random.seed(30)  #random seed
 
 with open("data.pkl", 'rb') as fs:
     data = pickle.load(fs)
 
 (trainMat, _, trustMat, categoryMat, categoryDict) = data
-
 trainMat=trainMat.tocsr()
-
 userNum, itemNum = trainMat.shape
 
+print(datetime.datetime.now())
+########################user-item distance matrix###############################
+UiDistance_mat = (sp.dok_matrix((userNum+itemNum, userNum+itemNum))).tocsr()
+UiDistance_mat[:userNum, userNum:] = trainMat
+UiDistance_mat[userNum:, :userNum] = trainMat.T
 
-#UIU
-UIU_mat = sp.dok_matrix((userNum, userNum))
-for i in tqdm(range(userNum)):
-    data = trainMat[i].toarray()
-    _, iid = np.where(data != 0)
-    uidList, _ = np.where(np.sum(trainMat[:, iid]!=0, axis=1) != 0)
-    uidList = uidList.tolist()
-    tmp = [i] * len(uidList)
-    UIU_mat[tmp, uidList] = 1
+########################user distance matrix###############################
+UserDistance_mat = sp.dok_matrix((userNum, userNum))
+#UU
+tmp_trustMat = trustMat.tocoo()
+uidList1, uidList2 = tmp_trustMat.row, tmp_trustMat.col
+UserDistance_mat[uidList1, uidList2] = 1.0
+UserDistance_mat[uidList2, uidList1] = 1.0
+##final result
+UserDistance_mat = (UserDistance_mat + sp.eye(userNum)).tocsr()
+with open('./UserDistance_mat.pkl', 'wb') as fs:
+    pickle.dump(UserDistance_mat, fs)
 
-UIU_mat = UIU_mat.tocsr()
-UIU_mat = UIU_mat + UIU_mat.T + sp.eye(userNum).tocsr()  
-UIU_mat = (UIU_mat != 0)
-
-
-#UITIU
-UITIU_mat = sp.dok_matrix((userNum, userNum))
-for i in tqdm(range(userNum)):
-    data = trainMat[i].toarray()
-    _, iid = np.where(data != 0)
-    typeidList = categoryMat[iid].toarray().reshape(-1)
-    typeidSet = set(typeidList.tolist())
-    for typeid in typeidSet:
-        iid2 = categoryDict[typeid]
-        uidList, _ = np.where(np.sum(trainMat[:, iid2]!=0, axis=1) != 0)
-        uidList2 = np.random.choice(uidList, size=int(uidList.size*0.1), replace=False)
-        uidList2 = uidList2.tolist()
-        tmp = [i]*len(uidList2)
-        UITIU_mat[tmp, uidList2] = 1
-UITIU_mat = UITIU_mat.tocsr()
-UITIU_mat = UITIU_mat + UITIU_mat.T + sp.eye(userNum).tocsr()  
-UITIU_mat = (UITIU_mat != 0)
-
-
-
-
-
-#IUI
-IUI_mat = sp.dok_matrix((itemNum, itemNum))
-trainMat_T = trainMat.T
+########################item distance matrix###############################
+ItemDistance_mat = sp.dok_matrix((itemNum, itemNum))
+#ITI
 for i in tqdm(range(itemNum)):
-    data = trainMat_T[i].toarray()
-    _, uid = np.where(data != 0)
-    iidList, _ = np.where(np.sum(trainMat_T[:, uid] != 0, axis=1) != 0)
-    iidList2 = np.random.choice(iidList, size=int(iidList.size*0.1), replace=False)
-    iidList2 = iidList2.tolist()
-    tmp = [i]*len(iidList2)
-    IUI_mat[tmp, iidList2] = 1
-IUI_mat = IUI_mat.tocsr()
-IUI_mat = IUI_mat + IUI_mat.T + sp.eye(itemNum).tocsr() 
-IUI_mat = (IUI_mat != 0)
-
-
+    itemType = categoryMat[i,0] #type id
+    itemList = categoryDict[itemType]
+    itemList = np.array(itemList)
+    itemList2 = np.random.choice(itemList, size=int(itemList.size*0.01), replace=False)
+    itemList2 = itemList2.tolist()
+    tmp = [i]*len(itemList2)
+    ItemDistance_mat[tmp, itemList2] = 1.0
+    ItemDistance_mat[itemList2, tmp] = 1.0
+##final result
+ItemDistance_mat = (ItemDistance_mat + sp.eye(itemNum)).tocsr()
+with open('./ItemDistance_mat.pkl', 'wb') as fs:
+    pickle.dump(ItemDistance_mat, fs)
+    
 print(datetime.datetime.now())
 
 metaPath = {}
